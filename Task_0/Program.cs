6df720f3265633533b6d8536d7a3204819923da2
Narek <narekazibekyan5@gmail.com>
2022-05-01 22:10:37 +0400

    Add project Task_0

---
+++ Task_0/Program.cs
@@ -0,0 +1,7 @@
+﻿
+Console.Write("Введите число -> ");
+int num = int.Parse(Console.ReadLine());
+int numResult = num * num;
+Console.Write("Квадрат числа равен -> ");
+Console.WriteLine(numResult);
+Console.ReadKey();
