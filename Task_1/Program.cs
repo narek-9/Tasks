76a3d356a9f2f95908533ee16c2c00a3bde15f3a
Narek <narekazibekyan5@gmail.com>
2022-05-01 22:29:13 +0400

    Add project Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число 1 -> ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2 -> ");
+int num2 = int.Parse(Console.ReadLine());
+bool result = num2 * num2 == num1;
+Console.WriteLine(result ? "Да" : "Нет");
+
