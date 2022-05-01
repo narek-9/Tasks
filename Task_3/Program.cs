5a4f0c7a6fd0038bde74427366247f713d6dbb88
Narek <narekazibekyan5@gmail.com>
2022-05-01 22:43:00 +0400

    Add project Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,9 @@
+﻿string dayWeek = Console.ReadLine();
+if(dayWeek == "1") Console.WriteLine("Понедельник");
+else if (dayWeek == "2") Console.WriteLine("Вторник");
+else if (dayWeek == "3") Console.WriteLine("Среда");
+else if (dayWeek == "4") Console.WriteLine("Четверг");
+else if (dayWeek == "5") Console.WriteLine("Пятница");
+else if (dayWeek == "6") Console.WriteLine("Суббота");
+else if (dayWeek == "7") Console.WriteLine("Воскресенье");
+else Console.WriteLine("Ошибка ввода!"); 
