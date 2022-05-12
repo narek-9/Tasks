c4d6d68a802d8ecd3c234407b73777281d59bd3e
Narek <narekazibekyan5@gmail.com>
2022-05-12 17:35:00 +0400

    Add Task_9

---
+++ Task_9/Program.cs
@@ -0,0 +1,8 @@
+﻿
+int number = new Random().Next(10, 100);
+Console.WriteLine($"Number = {number}");
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+Console.WriteLine($"Number 1 {firstDigit}");
+Console.WriteLine($"Number 2 {secondDigit}");
+Console.WriteLine(firstDigit > secondDigit ? $"Max = {firstDigit}" : $"Max = {secondDigit}");
