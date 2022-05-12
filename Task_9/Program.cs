181802011bec8ddba7b26d5265e94d216866eee8
Narek <narekazibekyan5@gmail.com>
2022-05-12 17:47:35 +0400

    Add Task_9 method

--- Task_9/Program.cs
+++ Task_9/Program.cs
@@ -1,8 +1,12 @@
 ﻿
 int number = new Random().Next(10, 100);
 Console.WriteLine($"Number = {number}");
-int firstDigit = number / 10;
-int secondDigit = number % 10;
-Console.WriteLine($"Number 1 {firstDigit}");
-Console.WriteLine($"Number 2 {secondDigit}");
-Console.WriteLine(firstDigit > secondDigit ? $"Max = {firstDigit}" : $"Max = {secondDigit}");
+int maxNum = FindMaxNumber (number);
+Console.WriteLine($"Max = {maxNum}");
+
+int FindMaxNumber (int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
