3547e2140ae95d5bfdd137851dca730e5c0d62ec
Narek <narekazibekyan5@gmail.com>
2022-05-12 18:13:40 +0400

    Add Task_11

---
+++ Task_11/Program.cs
@@ -0,0 +1,6 @@
+﻿int number = new Random().Next(100, 1000);
+int last = number % 10;
+int first = number / 10;
+int res = first * 10 + last;
+Console.WriteLine($"Случайное число = {number}");
+Console.WriteLine($"Результат = {res}");
