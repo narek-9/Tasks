b17e42dfab00c048de21f5510992027d97d8ea7f
Narek <narekazibekyan5@gmail.com>
2022-05-12 18:41:55 +0400

    Add Task_13

---
+++ Task_13/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Enter number A :");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Enter number B :");
+int b = int.Parse(Console.ReadLine());
+if (a % b == 0) Console.WriteLine("Кратно");
+else Console.WriteLine($"Не кратно, Остаток - {a % b}");
+Console.ReadKey();
