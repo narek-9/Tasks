869aac96fadef977df67f0f33d61db2369c38d9f
Narek <narekazibekyan5@gmail.com>
2022-05-12 19:16:40 +0400

    Add Task_16

---
+++ Task_16/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Enter number A :");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Enter number B :");
+int b = int.Parse(Console.ReadLine());
+bool res = b == a * a || a == b * b;
+Console.WriteLine(res ? "Да" : "Нет");
