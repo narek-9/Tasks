5a99c0cc97cc0869f9214948ef73d6e07646e9c3
Narek <narekazibekyan5@gmail.com>
2022-05-16 23:01:50 +0400

    Add Task_22

---
+++ Task_21/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите координаты первой точки ");
+double x1 = double.Parse(Console.ReadLine());
+double y1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки ");
+double x2 = double.Parse(Console.ReadLine());
+double y2 = double.Parse(Console.ReadLine());
+
+double distance = Math.Sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
+
+Console.WriteLine(distance);
---
+++ Task_22/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int i = 1;
+
+while (i <= number)
+{
+    int x = i * i;
+    Console.Write($"{x}, ");
+    i++;
+}
