7b222745d04fa5d8162b7bc4f91b77bd1a28f912
Narek <narekazibekyan5@gmail.com>
2022-05-16 22:06:58 +0400

    Add Task_18

---
+++ Task_18/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите номер четверти");
+var number = Console.ReadLine();
+
+if (number == "1")
+{
+    Console.WriteLine("x > 0 и y > 0");
+}
+else if (number == "2")
+{
+    Console.WriteLine("x < 0 и y > 0");
+}
+else if (number == "3")
+{
+    Console.WriteLine("x < 0 и y < 0");
+}
+else if (number == "4")
+{
+    Console.WriteLine("x > 0 и y < 0");
+}
+else Console.WriteLine("Вы ввели некорректные значения");
