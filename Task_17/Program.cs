4b817cb8bc3936bf3f7874eea2b4457a9fa21eb3
Narek <narekazibekyan5@gmail.com>
2022-05-16 21:39:26 +0400

    Add Task_17

---
+++ Task_17/Program.cs
@@ -0,0 +1,21 @@
+﻿
+Console.WriteLine("Введите координату X: ");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату Y: ");
+int y = int.Parse(Console.ReadLine());
+if(x > 0 && y > 0)
+{
+    Console.WriteLine("Диапазон находится в 1 четверти");
+}
+else if (x < 0 && y > 0)
+{
+    Console.WriteLine("Диапазон находится во 2 четверти");
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine("Диапазон находится в 3 четверти");
+}
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine("Диапазон находится в 4 четверти");
+}
