f11cd4ec1873fede60061defe13b50966ab27947
Narek <narekazibekyan5@gmail.com>
2022-05-19 23:14:02 +0400

    Add Task-30

---
+++ Task_30/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+int[] array = new int[8];
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(0, 2);
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int size = col.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{col[1]},");
+    }
+}
+
+FillArray(array);
+PrintArray(array);
