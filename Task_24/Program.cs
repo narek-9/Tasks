0eeb6452b04b4d06387611748e3b02821801678d
Narek <narekazibekyan5@gmail.com>
2022-05-19 21:34:18 +0400

    Add Task_24

---
+++ Task_24/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите натуральное число: ");
+int num = int.Parse(Console.ReadLine());
+
+
+
+int GetSumNumbers(int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum = sum + i;
+    }
+    return sum; 
+}
+
+int sumResult = GetSumNumbers(num);
+
+Console.WriteLine($"Сумма чисел от 1 до {num} = {sumResult}");
