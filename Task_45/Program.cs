f305018b8d673164ba561d39b81deb34a3c886aa
Narek <narekazibekyan5@gmail.com>
2022-05-26 23:23:49 +0400

    Add Task_45

---
+++ Task_45/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая будет создавать
+// копию заданного массива с помощью поэлементного
+// копирования.
+
+Console.WriteLine("Введите целое число: ");
+
+int size = int.Parse(Console.ReadLine());
+int[]array = new int[size];
+int[] newArray = new int[size];
+int sum = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 20);
+}
+for (int i = 0; i < array.Length; i++)
+{
+    newArray[i] = array[i];
+}
+
+Console.WriteLine($"{string.Join(' ',array)}");
+Console.WriteLine($"{string.Join(' ',newArray)}");
