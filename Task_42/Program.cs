13b1a20f460fec936a54e097ac6af14e1daffc5b
Narek <narekazibekyan5@gmail.com>
2022-05-26 22:36:12 +0400

    Add Task_42

---
+++ Task_42/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+// Console.WriteLine("Введите число: ");
+// int num = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите основание системы счисления: ");
+// int baseNum = int.Parse(Console.ReadLine());
+
+// int result = 0;
+// int d10 = 1;
+// while (num !=0)
+// {
+//     result = result + num % baseNum * d10;
+//     num /= 2;  // num = num / 2
+//     d10 *= 10; // d10 = d10 * d10
+// }
+
+// Console.WriteLine(result);
+
+void DecToOther (int n, int baseN)
+{
+    if (n == 0) return;
+    DecToOther (n / baseN, baseN);
+    Console.Write(n % baseN);
+}
+
+DecToOther(num, baseNum);
