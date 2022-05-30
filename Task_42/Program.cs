7667781b9037bba9a6e578ad837e49dc175acd72
Narek <narekazibekyan5@gmail.com>
2022-05-30 15:06:54 +0400

    Task_42

--- Task_42/Program.cs
+++ Task_42/Program.cs
@@ -4,21 +4,21 @@
 // 3 -> 11
 // 2 -> 10
 
-// Console.WriteLine("Введите число: ");
-// int num = int.Parse(Console.ReadLine());
-// Console.WriteLine("Введите основание системы счисления: ");
-// int baseNum = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите основание системы счисления: ");
+int baseNum = int.Parse(Console.ReadLine());
 
-// int result = 0;
-// int d10 = 1;
-// while (num !=0)
-// {
-//     result = result + num % baseNum * d10;
-//     num /= 2;  // num = num / 2
-//     d10 *= 10; // d10 = d10 * d10
-// }
+int result = 0;
+int d10 = 1;
+while (num !=0)
+{
+    result = result + num % baseNum * d10;
+    num /= 2;  // num = num / 2
+    d10 *= 10; // d10 = d10 * d10
+}
 
-// Console.WriteLine(result);
+Console.WriteLine(result);
 
 void DecToOther (int n, int baseN)
 {
