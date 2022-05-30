c2ccf0e2c7c54f898633b911b26f8cf147ecd67a
Narek <narekazibekyan5@gmail.com>
2022-05-30 22:18:06 +0400

    Add Task_48

---
+++ Task_48/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте двумерный массив размера m на n,
+// каждый элемент в массиве находится по формуле: Aₙₙ = m+n.
+// Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+Console.WriteLine("Введите кол-во строк: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите кол-во столбцов: ");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] array = new int[rows, columns];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = i+ j;
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintArray(array);
