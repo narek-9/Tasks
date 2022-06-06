94b4f559ea2f80fd581bac228c95cb9b0328aa16
Narek <narekazibekyan5@gmail.com>
2022-06-06 20:04:00 +0400

    Add Task_67

---
+++ Task_67/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+SumDigit(n, 0);
+void SumDigit(int num, int sum)
+{
+    if (num == 0)
+    {
+        Console.WriteLine(sum);
+        return;
+    }
+
+    sum = sum + num % 10;
+    num = num / 10;
+    SumDigit(num, sum);
+}
