c2fd7cbedd8d834bfeab9a3d2d8503dcc305eba2
Narek <narekazibekyan5@gmail.com>
2022-06-06 18:33:40 +0400

    Add Task_63

---
+++ Task_63/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+NaturalNumber(n);
+
+Console.ReadKey();
+
+void NaturalNumber(int num)
+{
+    if (num == 0) return;
+    NaturalNumber(num - 1);
+    Console.WriteLine($"{num} ");
+}
